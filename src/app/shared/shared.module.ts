fcda7ff9186b980adfdcf9598918bc85b5ff92cd
Gustavo Bonfim <gunais1234@gmail.com>
2020-02-23 13:40:14 -0300

    deleted ngx-bts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NgxBootstrapModule } from '../ngx-bootstrap/ngx-bootstrap.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    NgxBootstrapModule,
     CommonModule
   ],
   exports: [
-    NgxBootstrapModule
+    
   ]
 })
 export class SharedModule { }
