77c223bd23adab5826e54c932c9e2ee435013fe3
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-07-23 16:37:54 -0300

    app: container just if is user

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  { path: '', component: AdminComponent },
+  { path: 'empresas', component: EmpresasComponent },
+  { path: 'clientes', component: ClientesComponent }
 ];
 
 @NgModule({
