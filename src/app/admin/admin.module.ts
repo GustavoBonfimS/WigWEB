f39ae44ffab2a818428077c9967804bca37bf10f
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-07-23 16:26:44 -0300

    admin: menu, clientes/ empresas component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    EmpresasComponent,
+    ClientesComponent
   ],
   imports: [
     CommonModule,
