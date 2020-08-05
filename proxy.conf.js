87e423fa45e1bad9ef448f1bf59323443bf12bb1
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-08-04 21:56:21 -0300

    proxy conf: changeOrigin = true

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,6 +2,7 @@ const PROXY_CONFIG = [
     {
         context: ['/api'],
         target: 'http://localhost:8080',
+        changeOrigin: true,
         secure: false,
         logLevel: 'debug'
         //pathRewrite: { '^/api':  ''  }
