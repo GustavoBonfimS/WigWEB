95506508cfa4724c54c30ce1eef0fcba237f93a1
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-08-05 11:57:37 -0300

    bugfix: removing respostas on module and routing

--- src/app/minha-conta/minha-conta-routing.module.ts
+++ src/app/minha-conta/minha-conta-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MinhaContaComponent } from './minha-conta.component';
 import { AvaliacoesComponent } from './avaliacoes/avaliacoes.component';
-import { RespostasComponent } from './respostas/respostas.component';
 
 const routes: Routes = [
   { path: '', component: MinhaContaComponent, children: [
-    { path: 'avaliacoes', component: AvaliacoesComponent },
-    { path: 'respostas', component: RespostasComponent },
+    { path: 'avaliacoes', component: AvaliacoesComponent }
   ]},
 ];
 
--- src/app/minha-conta/minha-conta.module.ts
+++ src/app/minha-conta/minha-conta.module.ts
@@ -3,12 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AvaliacoesComponent } from './avaliacoes/avaliacoes.component';
 import { MinhaContaComponent } from './minha-conta.component';
 import { MinhaContaRoutingModule } from './minha-conta-routing.module';
-import { RespostasComponent } from './respostas/respostas.component';
-
-
 
 @NgModule({
-  declarations: [AvaliacoesComponent, MinhaContaComponent, RespostasComponent],
+  declarations: [AvaliacoesComponent, MinhaContaComponent],
   imports: [
     CommonModule,
     MinhaContaRoutingModule
