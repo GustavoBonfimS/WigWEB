e7ee92bf2324e026c1527ed704f183590eef8ef8
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-08-21 12:44:26 -0300

    avaliacao/perfil: exclusão de avaliacao implantada

--- src/app/empresas/perfil/perfil.component.ts
+++ src/app/empresas/perfil/perfil.component.ts
@@ -20,54 +20,83 @@ export class PerfilComponent implements OnInit {
   avaliacoes$: Observable<Empresa[]>;
   cliente: Cliente;
 
-  constructor(private methods: MethodsService,
-              private activateRoute: ActivatedRoute,
-              private modalService: AlertModalService,
-              private clienteCacheData: ClienteCacheDataService) { }
+  constructor(
+    private methods: MethodsService,
+    private activateRoute: ActivatedRoute,
+    private modalService: AlertModalService,
+    private clienteCacheData: ClienteCacheDataService
+  ) { }
 
   ngOnInit(): void {
     this.empresa = new Empresa();
     this.activateRoute.params
-    .pipe(
-      take(1),
-      switchMap(params => {
-        return this.methods.getEmpresaPeloNome(params.nome);
-      })
-    )
-    .subscribe(
-      empresa => {
-        this.empresa = empresa;
-        this.avaliacoes$ = this.methods.getAvaliacoesEmpresas(this.empresa.idempresa);
-      }
-    );
+      .pipe(
+        take(1),
+        switchMap(params => {
+          return this.methods.getEmpresaPeloNome(params.nome);
+        })
+      )
+      .subscribe(
+        empresa => {
+          this.empresa = empresa;
+          this.avaliacoes$ = this.methods.getAvaliacoesEmpresas(this.empresa.idempresa);
+        }
+      );
 
     this.cliente = this.clienteCacheData.getClienteLogado();
   }
 
   fazerAvaliacao() {
     this.modalService.showRatingModal(this.empresa)
-    .pipe(
-      take(1),
-      switchMap(message => {
-        if (message != null) {
-          const avaliacao = new Avaliacao();
-          avaliacao.autor = this.cliente.login;
-          avaliacao.conteudo = message;
-          avaliacao.idcliente = this.cliente.idcliente;
-          avaliacao.idempresa = this.empresa.idempresa;
-          return this.methods.insertAvaliacao(avaliacao);
-        } else {
-          return EMPTY;
-        }
-      })
-    )
-    .subscribe(response => {
+      .pipe(
+        take(1),
+        switchMap(message => {
+          if (message != null) {
+            const avaliacao = new Avaliacao();
+            avaliacao.autor = this.cliente.login;
+            avaliacao.conteudo = message;
+            avaliacao.idcliente = this.cliente.idcliente;
+            avaliacao.idempresa = this.empresa.idempresa;
+            return this.methods.insertAvaliacao(avaliacao);
+          } else {
+            return EMPTY;
+          }
+        })
+      )
+      .subscribe(response => {
         if (response != null) {
           this.modalService.showAlertSuccess('Enviado com sucesso!');
           this.avaliacoes$ = this.methods.getAvaliacoesEmpresas(this.empresa.idempresa);
         }
       }
-    );
+      );
+  }
+
+  showDeleteButton(idcliente: number) {
+    if (idcliente === this.clienteCacheData.getClienteLogado().idcliente) {
+      return true;
+    }
+    return false;
+  }
+
+  onDelete(idavaliacao: number) {
+    const title = 'Excluir avaliação?';
+    const msg = 'Tem certeza que deseja excluir a avaliação?';
+    this.modalService.showConfirm(title, msg)
+      .pipe(
+        take(1),
+        switchMap((res: boolean) => {
+          if (res) {
+            return this.methods.deleteAvaliacao(idavaliacao);
+          }
+          return EMPTY;
+        })
+      ).subscribe((result: boolean) => {
+        if (result) {
+          this.modalService.showAlertSuccess('Excluido com sucesso!');
+          this.avaliacoes$ = this.methods.getAvaliacoesEmpresas(this.empresa.idempresa);
+        }
+      });
   }
 
 }
