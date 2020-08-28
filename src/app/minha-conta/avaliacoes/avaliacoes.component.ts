18eb0e64f912256f080010a544e339f5d5185172
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2020-08-27 21:36:53 -0300

    minha-conta/avaliacoes: btn excluir

--- src/app/minha-conta/avaliacoes/avaliacoes.component.ts
+++ src/app/minha-conta/avaliacoes/avaliacoes.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MethodsService } from 'src/app/shared/methods.service';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
 import { Avaliacao } from 'src/app/shared/classes/Avaliacao';
 import { ClienteCacheDataService } from 'src/app/shared/cache/cliente-cache-data.service';
-import { take } from 'rxjs/operators';
+import { take, switchMap } from 'rxjs/operators';
 import { AlertModalService } from 'src/app/shared/alert-modal/alert-modal.service';
 
 @Component({
@@ -34,5 +34,25 @@ export class AvaliacoesComponent implements OnInit {
           this.alertModalService.showInfoRatingModal(item[index], resposta);
         });
     });
+  }
+
+  onDelete(idavaliacao: number) {
+    const title = 'Excluir avaliação?';
+    const msg = 'Tem certeza que deseja excluir a avaliação?';
+    this.alertModalService.showConfirm(title, msg)
+      .pipe(
+        take(1),
+        switchMap((res: boolean) => {
+          if (res) {
+            return this.methods.deleteAvaliacao(idavaliacao);
+          }
+          return EMPTY;
+        })
+      ).subscribe((result: boolean) => {
+        if (result) {
+          this.alertModalService.showAlertSuccess('Excluido com sucesso!');
+          this.avaliacoes$ = this.methods.getMinhasAvaliacoes(this.clienteCacheService.getClienteLogado().idcliente);
+        }
+      });
   }
 }
